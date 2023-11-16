c3104c425360747e7d854355d6ce8736cdb3a95c
asoa1 <127538753+asoa1@users.noreply.github.com>
2023-11-16 15:21:56 +0100

    Update enhancer.py

--- enhancer.py
+++ enhancer.py
@@ -1,3 +1,5 @@
+import cv2
+import time
 from colorama import init, Fore, Style
 
 # Initialize colorama to work on Windows terminals as well
@@ -5,60 +7,47 @@ init()
 
 def print_banner():
     banner = """
-   _   _           _     _  __     __                  
-  | | | |         | |   (_)/ _|   / _|                 
-  | |_| | __ _ ___| |__  _| |_   | |_ ___  _ __ _   _  
-  |  _  |/ _` / __| '_ \| |  _|  |  _/ _ \| '__| | | | 
-  | | | | (_| \__ \ | | | | |    | || (_) | |  | |_| | 
-  |_| |_|\__,_|___/_| |_|_|_|    |_| \___/|_|   \__,_| 
+     _   _           _     _  __     __                  
+    | | | |         | |   (_)/ _|   / _|                 
+    | |_| | __ _ ___| |__  _| |_   | |_ ___  _ __ _   _  
+    |  _  |/ _` / __| '_ \| |  _|  |  _/ _ \| '__| | | | 
+    | | | | (_| \__ \ | | | | |    | || (_) | |  | |_| | 
+    |_| |_|\__,_|___/_| |_|_|_|    |_| \___/|_|   \__,_| 
     """
 
     print(Fore.YELLOW + Style.BRIGHT + banner + Style.RESET_ALL)
 
-def print_menu():
-    print("Select a color to change the terminal:")
-    print("1. Red")
-    print("2. Green")
-    print("3. Blue")
-    print("4. Cyan")
-    print("5. Magenta")
-    print("6. White")
-    print("0. Exit")
+def take_picture():
+    # Open the default camera (0 or -1 for the default camera, adjust if needed)
+    cap = cv2.VideoCapture(0)
 
-def change_color(choice):
-    if choice == "1":
-        return Fore.RED
-    elif choice == "2":
-        return Fore.GREEN
-    elif choice == "3":
-        return Fore.BLUE
-    elif choice == "4":
-        return Fore.CYAN
-    elif choice == "5":
-        return Fore.MAGENTA
-    elif choice == "6":
-        return Fore.WHITE
-    else:
-        return None
+    # Check if the camera opened successfully
+    if not cap.isOpened():
+        print("Error: Could not open camera")
+        return
 
-def beautify_terminal():
     print_banner()
 
+    picture_count = 1
     while True:
-        print_menu()
-        user_choice = input("Enter your choice (0-6): ")
+        ret, frame = cap.read()
 
-        if user_choice == "0":
+        if not ret:
+            print("Error: Can't receive frame (stream end?). Exiting...")
             break
 
-        selected_color = change_color(user_choice)
-        if selected_color:
-            print(selected_color + f"This is the {selected_color} color!" + Style.RESET_ALL)
-        else:
-            print("Invalid choice. Please select a valid option.")
+        # Save the captured frame/image
+        image_name = f"picture_{picture_count}.jpg"
+        cv2.imwrite(image_name, frame)
+        print(f"Picture {picture_count} captured!")
+        picture_count += 1
+
+        # Wait for 5 seconds before capturing the next image
+        time.sleep(5)
+
+    # Release the camera and close the window
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    beautify_terminal()
-
-
-
+    take_picture()
